62c15550088c81d7df428e1d4c946192c6dca961
Joah Gerstenberg <joahd@gbergfamily.com>
2014-06-11 22:27:31 -0500

    v1.3.0 - embracing jQuery custom events. Documentation to come next.

--- dist/superSlider.js
+++ dist/superSlider.js
@@ -1,11 +1,14 @@
 /*!
- * superSlider.js v1.2.1 (https://github.com/JoahG/superSlider.js)
+ * superSlider.js v1.3.0 (https://github.com/JoahG/superSlider.js)
  * Copyright 2014 Joah Gerstenberg
  * Licensed under MIT License (https://github.com/JoahG/superSlider.js/blob/gh-pages/LICENSE)
  */
 (function($) { 
 	$.fn.superSlider = function($scope) {
+		// Set $scope to empty object if undefined
 		$scope = $scope == undefined ? {} : $scope;
+
+		// Local variables for jQuery selecting/setup
 		var slider = this;
 		var lis = this.find('li');
 		var ul = this.find('ul:not(.pagination)');
@@ -16,12 +19,16 @@
 		var larrow_inactive_src = $scope['left-arrow-inactive-src'] ?  $scope['left-arrow-inactive-src'] : 'images/chevron-left-inactive.png';
 		var rarrow_inactive_src = $scope['right-arrow-inactive-src'] ?  $scope['right-arrow-inactive-src'] : 'images/chevron-right-inactive.png';
 		var pagination = undefined;
+
+		// $scope variables for dealing with global attrs
 		$scope.indices = lis.length;
 		$scope.index_shown = 0;
 		$scope.timer_speed = $scope.timer_speed ? $scope.timer_speed : 5000;
 		$scope.timer_on = $scope.timer_on === true || $scope.timer_on === 'true' ? $scope.timer_on : false;
 		$scope.pagination = $scope.pagination === true || $scope.pagination === 'true' ? $scope.pagination : false;
-		$scope.do_the_magic = function() {
+
+		// $scope.init() to check window width and appropriately react
+		$scope.init = function() {
 			if ($(window).width() < 767) {
 				$scope.magic_number = $scope['mobile_width'] ? $scope['mobile_width'] : 500;
 				$scope.left_offset = $scope['left_offset_mobile'] || 0;
@@ -30,14 +37,18 @@
 				$scope.left_offset = $scope['left_offset_desktop'] || 40;
 			}
 		}
+
+		// $scope.check_activity to add correct activity class names to arrows
 		$scope.check_activity = function() {
 			if ($scope.index_shown <= 0) { larrow.addClass('inactive').attr('src', larrow_inactive_src); }
 			if ($scope.index_shown >= $scope.indices-1) { rarrow.addClass('inactive').attr('src', rarrow_inactive_src); }
 			if ($scope.index_shown > 0) { larrow.removeClass('inactive').attr('src', larrow_src); }
 			if ($scope.index_shown < $scope.indices-1) { rarrow.removeClass('inactive').attr('src', rarrow_src); }
 		}
+
+		// $scope.goto_slide to goto any slide in the gallery
 		$scope.goto_slide = function(n) {
-			$scope.do_the_magic();
+			$scope.init();
 			ul.css('left', ($scope.left_offset + (n * (0-$scope.magic_number))).toString()+'px');
 			$scope.index_shown = n;
 			lis.removeClass('current');
@@ -48,35 +59,75 @@
 			}
 			$scope.check_activity();
 		}
+
+		// $scope.paginate to create pagination when $scope.pagination == true
 		$scope.paginate = function() {
 			slider.append('<ul class="pagination"></ul>');
 			pagination = slider.find('ul.pagination');
 			for (var i = 0; i < $scope.indices; i++) { pagination.append('<li></li>'); }
 			pagination.find('li:nth-child('+($scope.index_shown+1)+')').addClass('active');
-			slider.on('click', '.pagination li:not(.active)', function() { $scope.goto_slide($(this).index()); });
+			slider.on('click', '.pagination li:not(.active)', function() { 
+				if ($(this).index() > $scope.index_shown) {
+					slider.trigger('slideRight', $(this).index() - $scope.index_shown);
+				} else if ($(this).index() < $scope.index_shown) {
+					slider.trigger('slideLeft', $scope.index_shown - $(this).index());
+				}
+			});
 		}
+
+		// When the slider is ready...
 		slider.ready(function(){
+			// Clear any preexisting jQuery events
 			slider.off('click', '.right-arrow:not(.inactive)');
 			slider.off('click', '.left-arrow:not(.inactive)');
-			slider.on('click', '.right-arrow:not(.inactive)', function() { $scope.goto_slide($scope.index_shown+1) });
-			slider.on('click', '.left-arrow:not(.inactive)', function() { $scope.goto_slide($scope.index_shown-1) });
-			$scope.do_the_magic();
+			slider.off('slideRight');
+			slider.off('slideLeft');
+			slider.off('slideTo');
+
+			// Define the jQuery events for slider
+			slider.on('slideRight', function(e, n) {
+				return slider.trigger('slideTo', ($scope.index_shown + (n || 1) < $scope.indices ? $scope.index_shown + (n || 1) : $scope.index_shown));
+			});
+
+			slider.on('slideLeft', function(e, n) {
+				return slider.trigger('slideTo', ($scope.index_shown - (n || 1) >= 0 ? $scope.index_shown - (n || 1) : $scope.index_shown))
+			});
+
+			slider.on('click', '.right-arrow', function() { 
+				return slider.trigger('slideRight');
+			});
+
+			slider.on('click', '.left-arrow',  function() { 
+				slider.trigger('slideLeft');
+			});
+
+			slider.on('slideTo', function(e, n) {
+				return n !== undefined ? $scope.goto_slide(n) : false;
+			})
+
+			// Call init function to get window width
+			$scope.init();
+
+			// Setup-y stuff
 			lis.each(function(i,v) { $(v).css('left', i*$scope.magic_number); });
 			ul.on('click', "li:not(.current)", function(e){ e.preventDefault(); $scope.goto_slide($(this).index()); });
+
+			// Paginate slides if pagination == true
+			if ($scope.pagination) { 
+				$scope.paginate(); 
+			}
+
+			// TouchWipe-specific code
 			if ($.fn.touchwipe !== undefined) {
 				$(".slider").touchwipe({ 
-					wipeLeft: function() {
-						if (!(rarrow.hasClass('inactive'))) {
-							$scope.goto_slide($scope.index_shown+1);
-						}
-					}, wipeRight: function() {
-						if (!(larrow.hasClass('inactive'))) {
-							$scope.goto_slide($scope.index_shown-1);
-						}
-					} 
+					wipeLeft:  function() { slider.trigger('slideRight'); }, 
+					wipeRight: function() { slider.trigger('slideLeft');  } 
 				});
 			}
+
+			// Code for timer_on == true
 			if ($scope.timer_on) {
+
 				var timer_helper = function() {
 					if (!(rarrow.hasClass('inactive'))) {
 						$scope.goto_slide($scope.index_shown+1);
@@ -84,20 +135,20 @@
 						$scope.goto_slide(0);
 					}
 				}
+
 				var timer = setInterval(function(){
 					timer_helper();
 				}, $scope.timer_speed);
+
 				slider.on('mouseenter', function(){
 					clearInterval(timer);
 				});
+
 				slider.on('mouseleave', function(){
 					timer = setInterval(function(){
 						timer_helper();
 					}, $scope.timer_speed);
 				});
-			}
-			if ($scope.pagination) { 
-				$scope.paginate(); 
 			}
 		});
 	};
